00f1f860ef3b76c8e16b0e7f8ac22f3f592bff07
AprilianoPeramu <gaminggorse@gmail.com>
2023-03-10 21:52:33 +0800

    fix responsive for content and passing size prop

--- src/App.js
+++ src/App.js
@@ -2,69 +2,83 @@ import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Avatar from "./Avatar";
 
-
 let a = [
   {
     id: 1,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
   {
     id: 2,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
   {
     id: 3,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
   {
     id: 4,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
   {
     id: 5,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
   {
     id: 6,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
   {
     id: 7,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
   {
     id: 8,
-    img: require('./asset/Picture.png')
+    img: require("./asset/Picture.png"),
   },
 ];
 
- function App() {
- 
+function App() {
   const [data, setData] = useState(0);
+  const [size, setSize] = useState({ width: "", height: "" });
 
-  const width = useRef(window.innerWidth);
-
-  console.log(width);
+  // get dynamic size
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: window.innerWidth,
+    dynamicHeight: window.innerHeight,
+  });
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+      dynamicHeight: window.innerHeight,
+    });
+  };
 
   useEffect(() => {
+    window.addEventListener("resize", setDimension);
+
+    return () => {
+      window.removeEventListener("resize", setDimension);
+    };
+  }, [screenSize]);
+
+  // useEffect avatar
+  useEffect(() => {
     //Should Refresh page so it can be responsive
-    if (window.innerWidth >= 1024 ) {
-      
+    if (window.innerWidth >= 1024) {
       setData(6);
-      
-    } else if (window.innerWidth >= 425 && window.innerWidth <= 767 ) {
+      setSize({ width: "100px", height: "100px" });
+    } else if (window.innerWidth >= 425 && window.innerWidth <= 767) {
       setData(2);
-      
+      setSize({ width: "90px", height: "90px" });
     } else {
       setData(5);
+      setSize({ width: "50px", height: "50px" });
     }
-
-  },[data]);
-
+  }, [screenSize.dynamicWidth]);
 
   return (
-    <div className="App"> 
-        <Avatar variant='outline' img={a} data={data} b={a.length} />   
+    <div className="App">
+      <Avatar img={a} data={data} b={a.length} size={size} />
     </div>
   );
 }
--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -1,23 +1,33 @@
 import "./Avatar.css";
 
 const Avatar = (props) => {
-  
+  console.log(props.size);
   return (
-    
     <div className="App">
       <div className="container">
         <div className="card">
-      {props.img.map((item, i) => i < props.data && (
-      <div className="defaultClass">
-        <img style={{width: props.size}} className="pic"  src={item.img} alt={props.nama} />
-      </div>
-     ))}
-     <div className="oke" >
-             <h1>+{props.b - props.data}</h1>
-      </div>
+          {props.img.map(
+            (item, i) =>
+              i < props.data && (
+                <div className="defaultClass" key={item.id}>
+                  <img
+                    style={{
+                      width: props.size.width,
+                      height: props.size.height,
+                    }}
+                    className="pic"
+                    src={item.img}
+                    alt={props.nama}
+                  />
+                </div>
+              )
+          )}
+          <div className="oke">
+            <h1>+{props.b - props.data}</h1>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 };
 export default Avatar;
